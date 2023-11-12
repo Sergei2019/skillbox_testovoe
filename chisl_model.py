cb5b23ef33b026a3374cb53e1da8eb6fb39e33b3
Sergei Sobolev <46378306+Sergei2019@users.noreply.github.com>
2023-11-13 00:36:55 +0300

    skill box testovoe

--- chisl_model.py
+++ chisl_model.py
@@ -18,7 +18,7 @@ from sklearn.linear_model import LinearRegression
 G = 9.8
 v0 = 300
 
-a = 0
+a = 0.07
 b = 0
 alpha = 30
 
@@ -43,28 +43,24 @@ X = 0
 Y = 0
 T = v0 * N(sin(alpha * deg.scale_factor))/G
 t = 0
-dt = 0.01
+dt = 1 / T
 
 """ Метод Рунге-Кутта 4-го порядка """
 while t < T:
-    vx_values.append(Vx)
-    vy_values.append(Vy)
-    x_values.append(Vx)
-    y_values.append(Vy)
-    time.append(t)
+
     dVx1 = (-a * sin(alpha * deg.scale_factor) * Vx - b * sin(alpha * deg.scale_factor) * sqrt(Vx**2 + Vy**2) * Vx) * dt
-    dVy1 = (-sin(alpha * deg.scale_factor) - a * sin(alpha * deg.scale_factor) * Vy - b * sin(alpha * deg.scale_factor) * N(sqrt(Vx**2 + Vy**2) * Vy)) * dt
+    dVy1 = (-sin(alpha * deg.scale_factor) - a * sin(alpha * deg.scale_factor) * Vy - b * sin(alpha * deg.scale_factor) * sqrt(Vx**2 + Vy**2) * Vy) * dt
     dX1 = Vx/(2 * cos(alpha * deg.scale_factor))
     dY1 = 2 * Vy/(cos(alpha * deg.scale_factor))
     dVx2 = (-a * sin(alpha * deg.scale_factor) * (Vx + dVx1/2) - b * sin(alpha * deg.scale_factor) * sqrt((Vx + dVx1/2)**2 + (Vy + dVy1/2)**2) * (Vx + dVx1/2)) * dt
     dVy2 = (-sin(alpha * deg.scale_factor) - a * sin(alpha * deg.scale_factor) * (Vy + dVy1/2) - b * sin(alpha * deg.scale_factor) * sqrt((Vx + dVx1/2)**2 + (Vy + dVy1/2)**2) * (Vy + dVy1/2)) * dt
     dX2 = (Vx + dVx1/2)/(2 * cos(alpha * deg.scale_factor))
     dY2 = 2 * (Vy + dVy1/2)/(cos(alpha * deg.scale_factor))
-    dVx3 = (-a * sin(alpha * deg.scale_factor) * (Vx + dVx2/2) - b * sin(alpha * deg.scale_factor) * N(sqrt((Vx + dVx2/2)**2 + (Vy + dVy2/2)**2) * (Vx + dVx2/2))) * dt
+    dVx3 = (-a * sin(alpha * deg.scale_factor) * (Vx + dVx2/2) - b * sin(alpha * deg.scale_factor) * sqrt((Vx + dVx2/2)**2 + (Vy + dVy2/2)**2) * (Vx + dVx2/2)) * dt
     dVy3 = (-sin(alpha * deg.scale_factor) - a * sin(alpha * deg.scale_factor) * (Vy + dVy2/2) - b * sin(alpha * deg.scale_factor) * sqrt((Vx + dVx2/2)**2 + (Vy + dVy2/2)**2) * (Vy + dVy2/2)) * dt
     dX3 = (Vx + dVx2/2)/(2 * cos(alpha * deg.scale_factor))
     dY3 = 2 * (Vy + dVy2/2)/(cos(alpha * deg.scale_factor))
-    dVx4 = (-a * sin(alpha * deg.scale_factor) * (Vx + dVx3) - b * sin(alpha * deg.scale_factor) * N(sqrt((Vx + dVx3)**2 + (Vy + dVy3)**2) * (Vx + dVx3))) * dt
+    dVx4 = (-a * sin(alpha * deg.scale_factor) * (Vx + dVx3) - b * sin(alpha * deg.scale_factor) * sqrt((Vx + dVx3)**2 + (Vy + dVy3)**2) * (Vx + dVx3)) * dt
     dVy4 = (-sin(alpha * deg.scale_factor) - a * sin(alpha * deg.scale_factor) * (Vy + dVy3) - b * sin(alpha * deg.scale_factor) * sqrt((Vx + dVx3)**2 + (Vy + dVy3)**2) * (Vy + dVy3)) * dt
     dX4 = (Vx + dVx3)/(2 * cos(alpha * deg.scale_factor))
     dY4 = 2 * (Vy + dVy3)/(cos(alpha * deg.scale_factor))
@@ -72,11 +68,15 @@ while t < T:
     Vy += (dVy1 + 2*dVy2 + 2*dVy3 + dVy4) / 6
     X += (dX1 + 2*dX2 + 2*dX3 + dX4) / 6
     Y += (dY1 + 2*dY2 + 2*dY3 + dY4) / 6
-    
+    vx_values.append(Vx)
+    vy_values.append(Vy)
+    x_values.append(X)
+    y_values.append(Y)
+    time.append(t)
     t += dt
 
-print(vx_values)
-print(vy_values)
+# print(vx_values)
+# print(vy_values)
 print(x_values)
 print(y_values)
 """ 3.3 Найдите решение системы ОДУ в случае отсутствия сопротивления, то есть
